39edbb1af3a135d19cfe2d83dce110cff671cf2c
kwonmink <minkung3234@naver.com>
2022-05-30 18:10:29 +0900

    test

---
+++ Default.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>無題のページ</title>
+    <style type="text/css">
+    
+        #form1
+        {
+            width: 396px;
+            height: 325px;
+        }
+        
+    </style>
+    </head>
+
+
+<body>
+    <form id="form1" runat="server">   
+      
+        <asp:Button ID="Button1" runat="server" Text="検索" />&nbsp;<br />
+        <asp:TextBox ID="TextBox1" runat="server"/><br />
+        <asp:Label ID="Label1" runat="server" Text=""/><br /><br />
+        
+        
+        <asp:Table ID="Table1" runat="server" BorderColor="#3333CC" BorderWidth="1px" GridLines="Both">
+        </asp:Table>
+        
+        <br />
+        <asp:Button ID="Button2" runat="server" Text="追加" />&nbsp;<br />
+        <asp:Label ID="Label2" runat="server" Text="employeeid"/>&nbsp;
+        <asp:TextBox ID="TextBox2" runat="server"/><br />
+        <asp:Label ID="Label3" runat="server" Text="departcode"/>&nbsp;
+        <asp:TextBox ID="TextBox3" runat="server"/><br />
+        <asp:Label ID="Label4" runat="server" Text="employname"/>&nbsp;
+        <asp:TextBox ID="TextBox4" runat="server"/><br />
+        <asp:Label ID="Label5" runat="server" Text="loginid"/>&nbsp;
+        <asp:TextBox ID="TextBox5" runat="server"/><br />
+        <asp:Label ID="Label6" runat="server" Text="loginpass"/>&nbsp;
+        <asp:TextBox ID="TextBox6" runat="server"/><br />
+        
+        
+        <asp:Label ID="Label7" runat="server" Text=""/>&nbsp;
+        
+
+        
+    </form>
+    
+ 
+</body>
+
+</html>
---
+++ Default.aspx.vb
@@ -0,0 +1,136 @@
+﻿Imports System.Data.SqlClient
+
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim dbcon As SqlConnection
+        Dim dbcmd As SqlCommand
+        Dim dataread As SqlDataReader
+        Dim sqlStr As String
+        Dim strdEmployname As String
+        Dim InputStr1 As String
+        Dim tableRow As TableRow
+        Dim tableCell As TableCell
+
+        InputStr1 = TextBox1().Text
+        Label1.Text = InputStr1 + " が入力されました。"
+
+        dbcon = New SqlConnection("Data Source=DESKTOP-NN6RU3T;Integrated Security=SSPI;Initial Catalog=test")
+
+        'DBオープン
+        dbcon.Open()
+
+        'SQL文
+        sqlStr = "SELECT employeeid, employname FROM employee WHERE employname=" + "'" + InputStr1 + "'"
+
+        'ＳＱＬコマンド作成
+        dbcmd = New SqlCommand(sqlStr, dbcon)
+
+        'ＳＱＬ文実行
+        dataread = dbcmd.ExecuteReader()
+
+        'TABLE生成
+        tableRow = New TableRow()
+
+        tableCell = New TableCell()
+        tableCell.text = "employeeid"
+        tableRow.Cells.Add(tableCell)
+
+        tableCell = New TableCell()
+        tableCell.text = "employname"
+        tableRow.Cells.Add(tableCell)
+        Table1.Rows.Add(tableRow)
+
+        While dataread.Read()
+
+            strdEmployname = dataread(1).ToString
+
+            'TABLE生成
+            tableRow = New TableRow()
+
+            tableCell = New TableCell()
+            tableCell.text = dataread(0).ToString
+            tableRow.Cells.Add(tableCell)
+
+            tableCell = New TableCell()
+            tableCell.text = dataread(1).ToString
+            tableRow.Cells.Add(tableCell)
+
+            Table1.Rows.Add(tableRow)
+
+        End While
+
+        'クローズ処理
+        dataread.Close()
+        dbcmd.Dispose()
+        dbcon.Close()
+        dbcon.Dispose()
+
+    End Sub
+
+
+    Protected Sub Button2_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim conn As SqlConnection
+        Dim cmd As SqlCommand
+
+
+        Dim sqlQuery As String
+
+        Dim InputStr2 As String
+        Dim InputStr3 As String
+        Dim InputStr4 As String
+        Dim InputStr5 As String
+        Dim InputStr6 As String
+
+        InputStr2 = TextBox2().Text
+        InputStr3 = TextBox3().Text
+        InputStr4 = TextBox4().Text
+        InputStr5 = TextBox5().Text
+        InputStr6 = TextBox6().Text
+
+
+        conn = New SqlConnection("Data Source=DESKTOP-NN6RU3T;Integrated Security=SSPI;Initial Catalog=test")
+
+        conn.Open()
+
+        sqlQuery = "Insert into employee (employeeid, departcode, employname, loginid, loginpass) VALUES (@employeeid, @departcode, @employname, @loginid, @loginpass)"
+
+
+        cmd = New SqlCommand(sqlQuery, conn)
+
+
+        cmd.Parameters.Add("@employeeid", InputStr2)
+        cmd.Parameters.Add("@departcode", InputStr3)
+        cmd.Parameters.Add("@employname", InputStr4)
+        cmd.Parameters.Add("@loginid", InputStr5)
+        cmd.Parameters.Add("@loginpass", InputStr6)
+
+        cmd.ExecuteNonQuery()
+
+
+        Label7.Text = "追加完了！"
+
+        conn.Close()
+
+
+        
+
+
+
+       
+
+
+
+
+
+
+
+    End Sub
+
+
+
+
+   
+End Class
